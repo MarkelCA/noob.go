f9c704c7d7d2145b6d3f41bebab209f085ac0764
MarkelCA <cuestaarribas.markel@proton.me>
2023-07-02 15:19:34 +0200

    Advent Of Code day 1

---
+++ advent-of-code-2022/day-1/part1/part1.go
@@ -0,0 +1,51 @@
+package part1
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	currentCals := 0
+	biggerCals := 0
+
+	currentElk := 1
+	richestElk := 0
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if text == "" {
+			currentElk += 1
+
+			if currentCals > biggerCals {
+				richestElk = currentElk
+				biggerCals = currentCals
+			}
+
+			currentCals = 0
+
+		} else {
+			i, _ := strconv.Atoi(text)
+			currentCals += i
+		}
+
+	}
+
+	fmt.Printf("Elk: %v, Calories: %v\n", richestElk, biggerCals)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ advent-of-code-2022/day-1/part2/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	currentCals := 0
+	currentElk := 1
+	topThree := [3]int{}
+
+	scanner := bufio.NewScanner(f)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if text == "" {
+
+			if currentCals > topThree[2] {
+				if currentCals > topThree[1] {
+					if currentCals > topThree[0] {
+						swap(topThree[0:], currentCals)
+						topThree[0] = currentCals
+					} else {
+						swap(topThree[1:], currentCals)
+						topThree[1] = currentCals
+					}
+				} else {
+					topThree[2] = currentCals
+				}
+			}
+			currentElk += 1
+			currentCals = 0
+
+		} else {
+			i, _ := strconv.Atoi(text)
+			currentCals += i
+		}
+
+	}
+
+	fmt.Println(topThree)
+
+	sum := 0
+	for _, x := range topThree {
+		sum += x
+	}
+
+	fmt.Printf("Top three sum: %v\n", sum)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func swap(slice []int, val int) {
+	aux := 0
+	for i, x := range slice {
+		aux = x
+		slice[i] = val
+		val = aux
+	}
+}
